ea525ccb2109c1c75bccf9a6fd8cd2bb7f2236ae
yaniam <ianporto25@gmail.com>
2025-11-12 16:41:59 -0300

    fix redeploy

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+// Netlify redeploy tweak
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
